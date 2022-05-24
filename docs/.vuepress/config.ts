afaab897c533f1d94f876cbe1fa2869f3fdd1c3a
Augus <bramlye@outlook.com>
2022-05-24 11:50:44 +0800

    test: geojson

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ module.exports = {
   head: [
     ["link", {rel: "icon", href: "/logo/favicon.ico"}],
     ["script", {src: "https://cdn.jsdelivr.net/npm/cfga@1.0.3", async: true}],
-    ["script", {src: "https://embed.github.com/view/geojson/DrAugus/Augus/tree/master/docs/i18n/zh/life/trip/README.md"}]
+    ["script", {src: "https://embed.github.com/view/geojson/DrAugus/Augus/tree/test/docs/.vuepress/public/json/json.geojson"}]
   ],
   base: "/Augus/",
   lang: 'en-US',
