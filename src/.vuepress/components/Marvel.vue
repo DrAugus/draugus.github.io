4d7fe5c4bc06d989dfddee10734c35dc6b616bd0
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-10-27 16:46:30 +0800

    feat: marvel zh

--- src/.vuepress/components/Marvel.vue
+++ src/.vuepress/components/Marvel.vue
@@ -1,6 +1,21 @@
 <template>
 
-    <div v-for="(v, k, i) in movie">
+    <p>
+        <span @click="showLang(true, false)">中文</span> |
+        <span @click="showLang(false, true)">English</span>
+    </p>
+
+
+    <div v-if="showZH" v-for="(v, k, i) in movie.zh">
+        <h3> {{ v.title }} </h3>
+        <p> 
+            <span class="mcu-date"> {{ v.release_date }} </span>
+            <br>
+            {{ v.overview }} 
+        </p>
+    </div>
+
+    <div v-if="showEN" v-for="(v, k, i) in movie.en">
         <h3> {{ v.title }} </h3>
         <p> 
             <span class="mcu-date"> {{ v.release_date }} </span>
@@ -20,11 +35,16 @@ export default {
         return {
             movie: mcu.moive,
             tv: mcu.tv,
+            showZH: false,
+            showEN: true,
         };
     },
     methods: {
-
-    }
+        showLang(zh, en) {
+            this.showZH = zh;
+            this.showEN = en;
+        },
+    },
 };
 </script>
   
