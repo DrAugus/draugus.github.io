2eec13cf6e82fe5522347a6fd0617a849ecd67ba
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-11-21 11:14:03 +0800

    refactor: link page

--- src/.vuepress/components/LinksRecord.vue
+++ src/.vuepress/components/LinksRecord.vue
@@ -1,17 +1,23 @@
 <template>
 
-  <h2>Good Website</h2>
+  <p>
+    <a @click="showLv('all')">all</a>
+    <span v-for="i in 6">
+      | <a @click="showLv(i - 1)">lv{{ i - 1 }}</a>
+    </span>
+  </p>
+
+  <p>
+    <a @click="showTag('all')">all</a>
+    <span v-for="(item, index) in filterTags" :key="index">
+      <a @click="showTag(item)"> | {{ item }}</a>
+
+    </span>
+  </p>
+
 
   <ul>
-    <li v-for="(t,i) in allLinks.website.good">
-      <a v-bind:href="t.url">{{ t.name }}<span v-if="t.tip">: {{ t.tip }}</span></a>
-    </li>
-  </ul>
-
-  <h2>Backup</h2>
-
-  <ul>
-    <li v-for="(t,i) in allLinks.website.backup">
+    <li v-for="(t, i) in linkLv">
       <a v-bind:href="t.url">{{ t.name }}<span v-if="t.tip">: {{ t.tip }}</span></a>
     </li>
   </ul>
@@ -21,13 +27,54 @@
 <script>
 import allLinks from "../data/links.json";
 
+const allLinkLv = [
+  ...(allLinks.website.lv0),
+  ...(allLinks.website.lv1),
+  ...(allLinks.website.lv2),
+  ...(allLinks.website.lv3),
+  ...(allLinks.website.lv4),
+  ...(allLinks.website.lv5)
+]
+
+console.log(allLinkLv)
+
+
+const allTags = allLinkLv.map(obj => (obj.tag.split(',')))
+console.log(allTags)
+
+// es6 降维 arr.flat(depth)
+const filterTags = new Set(allTags.reduce((prev, curr) => (prev.concat(curr)), []))
+console.log(filterTags)
+
+
 export default {
   name: "LinksRecord",
   data() {
     return {
-      allLinks
+      linkLv: allLinkLv,
+      filterTags,
     };
-  }
+  },
+  methods: {
+    showLv(lv) {
+      console.log(lv)
+      if (lv == 'all') {
+        this.linkLv = allLinkLv
+        return
+      }
+      let str = 'lv' + lv
+      this.linkLv = allLinks.website[str]
+    },
+    showTag(tag) {
+      console.log(tag)
+      if (tag == 'all') {
+        this.linkLv = allLinkLv
+        return
+      }
+      let all = allLinkLv
+      this.linkLv = all.filter((ele => ele.tag.indexOf(tag) != -1))
+    },
+  },
 };
 </script>
 
