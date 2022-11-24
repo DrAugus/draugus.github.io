1ba0b4c18b9725079ba41108869a816c56508ee8
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-11-24 15:24:32 +0800

    test: nuxt-docus

--- nuxt-docus/app.config.ts
+++ nuxt-docus/app.config.ts
@@ -1,5 +1,26 @@
 export default defineAppConfig({
   docus: {
-    title: 'My Docs'
+    title: 'My Docs',
+    description: 'The best place to start your documentation.',
+    socials: {
+      github: 'nuxt-themes/docus',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      // logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      // iconLinks: [
+      //   {
+      //     href: 'https://nuxt.com',
+      //     icon: 'IconNuxtLabs'
+      //   }
+      // ]
+    }
   }
 })
