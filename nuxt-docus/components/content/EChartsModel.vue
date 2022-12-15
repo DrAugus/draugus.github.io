c79decfd617834b67c09988cd273303b6e007853
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-12-15 15:10:49 +0800

    feat: docus echarts

---
+++ nuxt-docus/components/content/EChartsModel.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <v-chart class="chart" :option="option" />
+
+</template>
+  
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { UniversalTransition } from 'echarts/features';
+import { PieChart, LineChart } from "echarts/charts";
+import {
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+
+use([
+  UniversalTransition,
+  CanvasRenderer,
+  PieChart,
+  LineChart,
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
+
+export default {
+  name: "EChartsModel",
+  components: {
+    VChart,
+  },
+  props:{
+    option: Object,
+  },
+  provide: {
+    [THEME_KEY]: "auto",
+  },
+  data() {
+    return {
+      option: this.option,
+    };
+  },
+};
+</script>
+  
+<style scoped>
+.chart {
+  height: 100vh;
+}
+</style>
+  
+<style>
+body {
+  margin: 0;
+}
+</style>
