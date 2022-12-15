9f01c3c1cae35370fd3cd6893f394929f1af1351
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2022-12-15 15:20:38 +0800

    feat: test echart line

---
+++ nuxt-docus/components/content/Discount.vue
@@ -0,0 +1,79 @@
+<template>
+
+    500-50?
+
+    <EChartsModel :option="option" />
+</template>
+  
+<script>
+import EChartsModel from "./EChartsModel.vue"
+
+const opt = {
+    title: {
+        text: 'Stacked Line'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['<= 800km 燃油', '> 800km 燃油', '基建']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [
+            "2022年2月5日前", "2022年2月5日起", "2022年3月5日起",
+            "2022年4月5日起", "2022年5月5日起", "2022年6月5日起",
+            "2022年7月5日起", "2022年8月5日起", "2022年9月5日起",
+            "2022年11月5日起"
+        ]
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: "<= 800km 燃油 ",
+            type: 'line',
+            stack: 'Total',
+            data: [0, 10, 20, 50, 60, 80, 100, 80, 60, 60],
+        },
+        {
+            name: "> 800km 燃油 ",
+            type: 'line',
+            stack: 'Total',
+            data: [0, 20, 40, 100, 120, 140, 200, 140, 120, 110],
+        },
+        {
+            name: "基建",
+            type: 'line',
+            stack: 'Total',
+            data: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50],
+        },
+    ]
+}
+
+export default {
+    name: "Discount",
+    components: {
+        EChartsModel,
+    },
+    data() {
+        return {
+            option: opt,
+        };
+    },
+};
+</script>
+  
