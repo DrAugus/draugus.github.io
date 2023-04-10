29e4f29271bf404619e07aee989081ace9306c18
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-04-10 16:23:53 +0800

    feat: ls py

--- script/list_all_file.py
+++ script/list_all_file.py
@@ -6,21 +6,27 @@ all_filename = []
 output_filename = '.augus_output'
 exclude_path = [
     '.github', '.vscode', '.gitignore',
-    'LICENSE', '.git', output_filename
+    'LICENSE', '.git',
+    'node_modules',
+    'dist', 'cache',
+    '.vitepress', '.nuxt', '.output',
+    output_filename
 ]
+root_dir = os.getcwd()
+sub_dir = "life/trip/china"
 
 
-def list_all_files(root_dir):
+def list_all_files(dirs):
     _files = []
 
     # 列出文件夹下的所有目录和文件
-    l1 = os.listdir(root_dir)
+    l1 = os.listdir(dirs)
     l1 = list(filter(lambda e: e not in exclude_path, l1))
     # print(l1)
     all_filename.append(l1)
 
     for i in range(0, len(l1)):
-        path = os.path.join(root_dir, l1[i])
+        path = os.path.join(dirs, l1[i])
         if os.path.isdir(path):
             _files.extend(list_all_files(path))
         if os.path.isfile(path):
@@ -29,9 +35,19 @@ def list_all_files(root_dir):
     return _files
 
 
-def format_line(s):
+def format_line(filename):
+    first_line = ''
+    # print(root_dir, filename)
+    with open(os.path.join(root_dir, sub_dir + filename), 'r') as f:
+        first_line = f.readline()
+        first_line = first_line[2:]
+        first_line = first_line.rstrip('\n')
+
     pre = 'url_life'
-    return pre + ' + "' + s + '",\n'
+    res = f"{{ text: '{first_line}', link: '{filename}' }},"
+    # res = pre + ' + "' + s + '",'
+    res += '\n'
+    return res
 
 
 def display(filename):
@@ -52,7 +68,9 @@ def prefix_dir(current_dir, arr):
 
 
 def write2file():
+    os.chdir(os.path.join(root_dir, sub_dir))
     current_dir = os.getcwd()
+
     lll = list_all_files(current_dir)
     lll = prefix_dir(current_dir, lll)
 
