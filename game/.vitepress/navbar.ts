48b0dc8f7b9a9d639c2894ef0770f7d77c7fdcef
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-04-10 10:38:38 +0800

    feat: pilot

--- game/.vitepress/navbar.ts
+++ game/.vitepress/navbar.ts
@@ -3,6 +3,7 @@ const url_prefix = '';
 const url_game = url_prefix + '/game';
 
 export const navbar_zh = [
+  { text: '导航', link: 'https://augusmeow.github.io/' },
   {
     text: "原神",
     link: url_game + "/genshin/",
