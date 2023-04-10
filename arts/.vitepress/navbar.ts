81ecafe7a3382376afa5dca552576c4b6b62c7e5
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-04-10 15:00:43 +0800

    docs

--- arts/.vitepress/navbar.ts
+++ arts/.vitepress/navbar.ts
@@ -1,4 +1,5 @@
 export const navbar = [
+    { text: '导航', link: 'https://augusmeow.github.io/' },
     { text: '语言', link: '/language/', activeMatch: '/language/' },
     { text: '经济学', link: '/economics/', activeMatch: '/economics/' },
     { text: '历史', link: '/history/', activeMatch: '/history/' },
