75030a5dc5b69012bb7b05737329ee0f011781a4
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-04-12 13:57:00 +0800

    fix: sidebar

--- life/.vitepress/sidebar.ts
+++ life/.vitepress/sidebar.ts
@@ -1,14 +1,8 @@
 export const sidebar = {
     '/eat/': [
-        {
-            text: '吃点儿',
-            collapsed: true,
-            items: [
-                { text: '真的不能吃', link: '/eat/block' },
-                { text: '做点什么吧', link: '/eat/cookbook' },
-                { text: '吃饭了', link: '/eat/choose' },
-            ],
-        }
+        { text: '真的不能吃', link: '/eat/block' },
+        { text: '做点什么吧', link: '/eat/cookbook' },
+        { text: '吃饭了', link: '/eat/choose' },
     ],
     '/trip/': [
         { text: '规划指南', link: '/trip/' },
