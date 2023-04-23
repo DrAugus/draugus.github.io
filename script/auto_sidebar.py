5fc24dcfd0e30d9f135f6d1e094c01a946688db0
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-04-23 18:33:01 +0800

    feat

--- script/auto_sidebar.py
+++ script/auto_sidebar.py
@@ -1,6 +1,5 @@
 import os
 
-list_file_depth = 2
 current_depth = 0
 all_filename = []
 output_filename = '.augus_output'
@@ -14,18 +13,42 @@ exclude_path = [
     # 'china','abroad'
 ]
 root_dir = os.getcwd()
-sub_dir = "life/trip/abroad"
+sub_dir = "life/code/"
 link_pre = "/trip/abroad"
 
-
 def list_all_files(dirs):
+    global current_depth
     _files = []
 
     # 列出文件夹下的所有目录和文件
     l1 = os.listdir(dirs)
     l1 = list(filter(lambda e: e not in exclude_path, l1))
-    # print(l1)
+    if current_depth == 0:
+        # print(l1)
+        depth1_dirs = []
+        depth1_path = []
+        for i in range(0, len(l1)):
+            path = os.path.join(dirs, l1[i])
+            if os.path.isdir(path):
+                depth1_path.append(path)
+                depth1_dirs.append(l1[i])
+        print(depth1_dirs)
+        print(depth1_path)
+
+    if current_depth == 1:
+        # print(l1)
+        depth2_dirs = []
+        depth2_path = []
+        for i in range(0, len(l1)):
+            path = os.path.join(dirs, l1[i])
+            if os.path.isdir(path):
+                depth2_path.append(path)
+                depth2_dirs.append(l1[i])
+        print(depth2_dirs)
+        print(depth2_path)
+        
     all_filename.append(l1)
+    current_depth = current_depth + 1
 
     for i in range(0, len(l1)):
         path = os.path.join(dirs, l1[i])
@@ -75,6 +98,8 @@ def write2file():
 
     lll = list_all_files(current_dir)
     lll = prefix_dir(current_dir, lll)
+
+    # print(all_filename)
 
     # 使用 with 关键字打开文件
     with open(output_filename, "w") as f:
