52d2c98552049486987adbc399f6726b1e85ee1e
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-04-26 17:25:44 +0800

    feat

---
+++ game/.vitepress/theme/components/AsideSponsors.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { VPDocAsideSponsors } from 'vitepress/theme'
+
+const data = [
+  {
+    items: [{ img: '/image/alipay.png' }]
+  },
+  {
+    items: [{ img: '/image/wechatpay.png' }]
+  }
+]
+</script>
+
+<template>
+  <VPDocAsideSponsors :data="data" />
+</template>
+
+<style>
+.vp-sponsor-grid.medium .vp-sponsor-grid-image {
+  max-height: 90px;
+}
+</style>
---
+++ life/.vitepress/theme/components/AsideSponsors.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { VPDocAsideSponsors } from 'vitepress/theme'
+
+const data = [
+  {
+    items: [{ img: '/img/alipay.png' }]
+  },
+  {
+    items: [{ img: '/img/wechatpay.png' }]
+  }
+]
+</script>
+
+<template>
+  <VPDocAsideSponsors :data="data" />
+</template>
+
+<style>
+.vp-sponsor-grid.medium .vp-sponsor-grid-image {
+  max-height: 90px;
+}
+</style>
