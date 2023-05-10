1fbcc861500b0d61afe971cf5e22f9eda7794f08
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-05-10 11:26:49 +0800

    feat: hsr wish info

--- game/.vitepress/components/hsr/wish.ts
+++ game/.vitepress/components/hsr/wish.ts
@@ -1,8 +1,9 @@
+// https://honkai-star-rail.fandom.com/wiki/
 // 20 + 21= 41 days one version /42days/
 export const WISH = {
   characters: [
     {
-      name: "",
+      name: "Butterfly on Swordtip",
       image: 1,
       shortName: "Seele",
       start: "2023-04-26 10:00:00 +0800",
@@ -16,13 +17,13 @@ export const WISH = {
   ],
   weapons: [
     {
-      name: "Invocation",
+      name: "Brilliant Fixation",
       image: 1,
       start: "2023-04-26 10:00:00 +0800",
       end: "2023-05-17 17:59:59 +0800",
-      shortName: "",
-      wish5star: ['aquila_favonia', 'amos_bow'],
-      wish4star: ['the_flute', 'the_bell', 'the_widsith', 'the_stringless', 'favonius_lance'],
+      shortName: "in_the_night",
+      wish5star: ['In the Night'],
+      wish4star: ['Post-Op Conversation', 'Good Night and Sleep Well', 'The Moles Welcome You'],
       url: ""
     },
   ]
