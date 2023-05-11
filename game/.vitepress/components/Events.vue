83509fbfe8640102cea2ef36524974e846d261ef
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-05-11 16:28:05 +0800

    fix: mobile

--- game/.vitepress/components/Events.vue
+++ game/.vitepress/components/Events.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="event-info">
         <div>
+            <h2>原神祈愿</h2>
             <GenshinWish />
         </div>
         <div>
+            <h2>星穹铁道跃迁</h2>
             <HsrWish />
         </div>
     </div>
@@ -28,6 +30,10 @@ import HsrWish from "./hsr/WishRecent.vue";
     .event-info {
         flex-direction: column;
     }
+
+    .event-info>div {
+        width: 100%;
+    }
 }
 
 .event-info {
@@ -46,4 +52,13 @@ import HsrWish from "./hsr/WishRecent.vue";
         padding: calc(var(--vp-nav-height) + var(--vp-layout-top-height, 0px) + 80px) 64px 64px;
     }
 }
+
+.bg-height {
+  height: 200px;
+}
+
+.f-w-600 {
+  font-weight: 600;
+}
+
 </style>
