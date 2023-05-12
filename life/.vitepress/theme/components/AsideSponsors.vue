440f5df2d34822b5144c1590fdb4cba0a17b65a7
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-05-12 18:21:13 +0800

    fix: base

--- life/.vitepress/theme/components/AsideSponsors.vue
+++ life/.vitepress/theme/components/AsideSponsors.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { VPDocAsideSponsors } from 'vitepress/theme'
 
+import { withBase } from 'vitepress'
+
 const data = [
   {
-    items: [{ img: '/img/alipay.png' }]
+    items: [{ img: withBase('/img/alipay.png') }]
   },
   {
-    items: [{ img: '/img/wechatpay.png' }]
+    items: [{ img: withBase('/img/wechatpay.png') }]
   }
 ]
 </script>
