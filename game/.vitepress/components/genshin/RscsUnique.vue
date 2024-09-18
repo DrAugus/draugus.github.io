a38601fa54d6b00f6aed293a4562d482f289a666
AugusMeow <bramlye@outlook.com>
2024-09-18 15:09:28 +0800

    feat(genshin): 区域特产

---
+++ game/.vitepress/components/genshin/RscsUnique.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import json from "../../data/genshin/rscs-unique.json";
+import { City } from "./utils";
+import { CHARACTER } from "./characters";
+
+let showZH = {};
+for (let city in json) {
+    let zhCity: string = City[city].name;
+    let zhCharas: string[] = []
+    for (let chara of json[city]) {
+        let zhChara: string = CHARACTER[chara].name;
+        zhCharas.push(zhChara);
+    }
+    if (!zhCharas.length) {
+        zhCharas.push("暂无")
+    }
+    showZH[zhCity] = zhCharas;
+}
+
+</script>
+<template>
+    <div v-for="(v, k, i) in showZH">
+        <h3>{{ k }}</h3>
+        <div v-for="(vv, ii) in v">
+            {{ vv }}
+        </div>
+    </div>
+</template>
